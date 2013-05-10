bf9c752e835b10a712e7bfe3dbb15837ebf5e4d5
Andrew Kelly <kellyaj@gmail.com>
2013-05-10 09:48:04 -0500

    cleanup of unused files

--- spec/eungdap/request_reader_spec.clj
+++
@@ -1,9 +0,0 @@
-(ns eungdap.request-reader-spec
-  (:require [speclj.core :refer :all]
-            [eungdap.request-reader :refer :all]))
-;(import '[java.io InputStream])
-;(describe "Request Reader"
-  ;(it "properly reads multiple lines"
-  ;  (binding [tester (InputStream. "abcdef")]
-  ;  (should= "abc\r\ndef"
-  ;    (read-request tester)))))
