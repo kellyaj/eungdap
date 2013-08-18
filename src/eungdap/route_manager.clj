d2410ee8b8ed9d9185c507e829f0347428c98177
Andrew Kelly <kellyaj@gmail.com>
2013-08-17 21:54:16 -0500

    removing unnecessary routing

--- src/eungdap/route_manager.clj
+++ src/eungdap/route_manager.clj
@@ -2,22 +2,11 @@
 
 (defn check-route-validity [requested-route]
   (contains? #{
-               "/redirect"
                "/"
                "/form"
                "/form.html"
-               "/pretzels"
-               "/text-file.txt"
-               "/file1"
-               "/file2"
-               "/pretzels.html"
-               "/index.html"
-               "/public"
-               "/image.jpeg"
-               "/image.gif"
-               "/partial_content.txt"
-               "/parameters"
-               "/image.png"} requested-route))
+               "/redirect"
+               "/parameters"} requested-route))
 
 (defn method-allowed? [route http-method]
   (cond
